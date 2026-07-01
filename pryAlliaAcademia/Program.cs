cb4e76d31ff2f073502daa5c9e551de8ba86c6ec
tomiallia <tomiallia@gmail.com>
2026-07-01 03:21:00 -0300

    ready

--- pryAlliaAcademia/Program.cs
+++ pryAlliaAcademia/Program.cs
@@ -16,7 +16,7 @@ namespace pryAlliaAcademia
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMain());
+            Application.Run(new frmInicio());
         }
     }
 }
